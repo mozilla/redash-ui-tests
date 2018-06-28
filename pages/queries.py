16937bd49bef2af7ea2e172bdd263944ead905ad
Raphael Pierzina <raphael@hackebrot.de>
2018-06-28 14:49:01 +0200

    Add type hints to queries.py

--- pages/queries.py
+++ pages/queries.py
@@ -2,39 +2,46 @@
 
 """Page model for the queries page."""
 
-from pypom import Page, Region
+import typing
 
+from pypom import Page, Region
 from selenium.webdriver.common.by import By
 
-
-class QueryPage(Page):
-
-    _query_table_locator = (By.TAG_NAME, "table")
-    _table_row_locator = (By.TAG_NAME, "tr")
-
-    @property
-    def queries(self):
-        table = self.selenium.find_element(*self._query_table_locator)
-        items = table.find_elements(*self._table_row_locator)
-        return [self.QueryRow(self, item) for item in items]
-
-    class QueryRow(Region):
-
-        _query_link_locator = (By.CSS_SELECTOR, "td a")
-
-        @property
-        def link(self):
-            return self.selenium.find_element(*self._query_link_locator)
-
-        def click(self):
-            self.link.click()
-            return QueryDetailPage(self.selenium)
+Selector = typing.Tuple[typing.Any, str]
 
 
 class QueryDetailPage(Page):
 
-    _query_description_locator = (By.CSS_SELECTOR, ".edit-in-place p")
+    _query_description_locator: Selector = (
+        By.CSS_SELECTOR,
+        ".edit-in-place p",
+    )
 
     @property
-    def description(self):
+    def description(self) -> typing.Any:
         return self.find_element(*self._query_description_locator).text
+
+
+class QueryRow(Region):
+
+    _query_link_locator: Selector = (By.CSS_SELECTOR, "td a")
+
+    @property
+    def link(self) -> typing.Any:
+        return self.selenium.find_element(*self._query_link_locator)
+
+    def click(self) -> QueryDetailPage:
+        self.link.click()
+        return QueryDetailPage(self.selenium)
+
+
+class QueryPage(Page):
+
+    _query_table_locator: Selector = (By.TAG_NAME, "table")
+    _table_row_locator: Selector = (By.TAG_NAME, "tr")
+
+    @property
+    def queries(self) -> typing.List[QueryRow]:
+        table = self.selenium.find_element(*self._query_table_locator)
+        items = table.find_elements(*self._table_row_locator)
+        return [self.QueryRow(self, item) for item in items]
