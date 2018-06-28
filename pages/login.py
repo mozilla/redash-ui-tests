7546e00d15992a5a0c13c24eb5f5c85f9af07a6b
Raphael Pierzina <raphael@hackebrot.de>
2018-06-28 14:29:52 +0200

    Add type hints to LoginPage

--- pages/login.py
+++ pages/login.py
@@ -2,55 +2,64 @@
 
 """Page object model for login."""
 
-from pypom import Page
+import typing
 
+from pypom import Page
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as expected
+
+from pages.home import HomePage
+
+Selector = typing.Tuple[typing.Any, str]
 
 
 class LoginPage(Page):
     """Page object model for the login."""
 
-    URL_TEMPLATE = "/{org}/login/"
+    URL_TEMPLATE: str = "/{org}/login/"
 
-    _alert_locator = (By.CSS_SELECTOR, ".alert-danger")
-    _input_email_locator = (By.ID, "inputEmail")
-    _input_password_locator = (By.ID, "inputPassword")
-    _login_buttton_locator = (By.CSS_SELECTOR, "button[type='submit']")
+    _alert_locator: Selector = (By.CSS_SELECTOR, ".alert-danger")
+    _input_email_locator: Selector = (By.ID, "inputEmail")
+    _input_password_locator: typing.Tuple[typing.Any, str] = (
+        By.ID,
+        "inputPassword",
+    )
+    _login_buttton_locator: Selector = (
+        By.CSS_SELECTOR,
+        "button[type='submit']",
+    )
 
     @property
-    def title(self):
+    def title(self) -> typing.Any:
         """Return the page title."""
         return self.wait.until(lambda s: self.selenium.title)
 
     @property
-    def alert(self):
+    def alert(self) -> typing.Any:
         """Return the alert element."""
         element = self.wait.until(
             expected.visibility_of_element_located(self._alert_locator)
         )
         return element.text
 
-    def enter_email(self, email):
+    def enter_email(self, email: str) -> None:
         """Enter the given user email."""
         input_email = self.find_element(*self._input_email_locator)
         input_email.send_keys(email)
 
-    def enter_password(self, password):
+    def enter_password(self, password: str) -> None:
         """Enter the given user password."""
         input_password = self.find_element(*self._input_password_locator)
         input_password.send_keys(password)
 
-    def click_login(self):
+    def click_login(self) -> None:
         """Click the login button."""
         btn = self.find_element(*self._login_buttton_locator)
         btn.click()
 
-    def login(self, email="", password=""):
+    def login(self, email: str = "", password: str = "") -> typing.Any:
         """Log in the user with the given credentials."""
         self.enter_email(email)
         self.enter_password(password)
         self.click_login()
-        from pages.home import HomePage
-
         return HomePage(self.selenium).wait_for_page_to_load()
